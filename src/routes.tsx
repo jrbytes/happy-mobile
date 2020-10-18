754a18111dce2d7bd84c02a3228ecbdeeead01c0
Jr Bytes <itamazor@gmail.com>
2020-10-18 15:55:34 -0300

    Added CreateOrphanage. Completed other pages.

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,13 +7,41 @@ const { Navigator, Screen } = createStackNavigator()
 
 import OrphanagesMap from './pages/OrphanagesMap'
 import OrphanagesDetails from './pages/OrphanageDetails'
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'
+import OrphanageData from './pages/CreateOrphanage/OrphanageData'
+
+import Header from './components/Header'
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
-        <Screen name='OrphanageMap' component={OrphanagesMap} />
-        <Screen name='OrphanageDetails' component={OrphanagesDetails} />
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+        <Screen name='OrphanagesMap' component={OrphanagesMap} />
+        <Screen
+          name='OrphanageDetails'
+          component={OrphanagesDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title='Orfanato' />
+          }}
+        />
+        
+        <Screen
+          name='SelectMapPosition'
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title='Selecione no mapa' />
+          }}
+        />
+        <Screen
+          name='OrphanageData'
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title='Informe os dados' />
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   )
